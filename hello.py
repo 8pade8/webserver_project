0cd323bca26ee1d2cd939a1885800cf1e123fe80
xRedFlame <smirnoshka@gmail.com>
2020-04-12 01:22:09 +0300

    fix errors

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def hello(environ, start_response):
     querystring = environ.get('QUERY_STRING')
-    body_response = querystring.split('&')
+    body_response = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     status = '200 OK'
     headers = [
     ('Content-Type', 'text/plain')
