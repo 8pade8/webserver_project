193cae2cf9e1de91fa7dd9f869c1455672d20096
xRedFlame <smirnoshka@gmail.com>
2020-04-18 19:48:51 +0300

    fix errors

--- init.sh
+++ init.sh
@@ -23,5 +23,5 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn --bind='0.0.0.0:8080' hello:hello
+sudo gunicorn --bind='0.0.0.0:8080' hello:hello &
 sudo gunicorn --bind='0.0.0.0:8000' ask.ask.wsgi:application &
